db977f0af1b9af225ee35e812ef22d4fea5bd431
Dawsonkw <dawsonkw@gmail.com>
2024-04-22 13:23:06 -0600

    refactored list items to use constants, added constants.js file, updated eslint config, added new rules for eslint, updated button group component to use constants

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,11 +1,31 @@
+import { initialItems } from "../lib/constants";
+import { useState } from "react";
+
+
 
 
 function ItemList() {
+    const [items, setItems] = useState(initialItems);
+
     return (
-        <div>
-            
-        </div>
+        <ul>
+            {
+                items.map(item => {
+                    return <Item key={item.id} item={item} />;
+                })
+            }
+        </ul>
     );
 }
 
-export default ItemList;
+function Item( { item } ) {
+    return <li className="item">
+                <label >
+                <input  type="checkbox" />
+                {item.name}
+                </label>
+                <button>❌</button>
+            </li>
+}
+
+export default (ItemList, Item);
---
+++ src/lib/constants.js
@@ -0,0 +1,29 @@
+
+export const initialItems = [
+    {
+        id: 1,
+        name: "good mood",
+        packed: true,
+    }, 
+    {
+        id: 2,
+        name: "passport",
+        packed: false,
+    }, 
+    {
+        id: 3,
+        name: "money",
+        packed: true,
+    },
+    {   
+        id: 4,
+        name: "phone charger",
+        packed: false,
+    }];
+    
+
+export const secondaryButtons = ['Mark all as complete',
+    'Mark all as incomplete', 
+    'Reset to initial', 
+    'Remove all items'
+   ];
